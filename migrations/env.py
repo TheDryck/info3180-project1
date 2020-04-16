b310c8f9f8a5f45c4050138e76a0ffbc709d9a97
TheDryck <rodgersdryck@gmail.com>
2020-04-15 22:36:55 -0500

    Done

--- migrations/env.py
+++ migrations/env.py
@@ -1,6 +1,6 @@
 from __future__ import with_statement
 from alembic import context
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import engine_from_config, poo
 from logging.config import fileConfig
 import logging
 
